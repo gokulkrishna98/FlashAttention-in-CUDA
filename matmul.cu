1cdb81d310308f46d7b0342d3e97ddf0d56fb46b
gokulkrishna98 <gokulkrishna98@gmail.com>
2025-02-16 00:31:27 -0500

    memory coalescing in matmul

--- matmul.cu
+++ matmul.cu
@@ -1,18 +1,21 @@
 #include <cuda_runtime.h>
-#include <algorithm>
 
 __global__
 void matmul(float *a, float *b, float *out, int ha, int wa, int hb, int wb, float sf){
+
+    // threads along x axis are group into warp
     int x = blockIdx.x*blockDim.x + threadIdx.x;
     int y = blockIdx.y*blockDim.y + threadIdx.y;
 
-    // wa = hb
-    if(x < ha && y < wb){
+    // matmul cond: wa = hb
+    // for memory coalescing the x should point to column and y to row.
+    // out[y, x] = row[y] * col[x];
+    if(x < wb && y < ha){
         float res = 0.0f;
         for(int i=0; i<wa; i++){
-            res += a[x*wa + i] * b[i*wb + y];
+            res += a[y*wa + i] * b[i*wb + x];
         }
-        out[x*wb + y] = (res * sf);
+        out[y*wb + x] = (res * sf);
     }
     return;
 }
@@ -21,11 +24,11 @@ void matmul(float *a, float *b, float *out, int ha, int wa, int hb, int wb, floa
 
 extern "C" void launch_matmul(float *a, float *b, float *out, int ha, int wa, int hb, int wb, float sf = 1.0f) {
     // ha x wb
-    int block_size_x = std::min(32, ha);
-    int block_size_y = std::min(32, wb);
+    int block_size_x = 32;
+    int block_size_y = 32;
 
     dim3 block_size(block_size_x, block_size_y);
-    dim3 grid_size((ha + block_size_x -1)/block_size_x, (wb + block_size_y -1)/block_size_y);
+    dim3 grid_size((wb + block_size_x -1)/block_size_x, (ha + block_size_y -1)/block_size_y);
     matmul<<<grid_size, block_size>>>(a, b, out, ha, wa, hb, wb, sf);
     return;
 }
